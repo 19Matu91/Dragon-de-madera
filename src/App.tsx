e5cd587c3041f62de5832fec6e201bfe7c554d1a
A Matutano <alejandro.matutano@evoluciona.com>
2026-01-10 21:44:31 +0100

    cambio en app

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { useLocation } from 'react-router-dom'
 import { AppRoutes } from '@/config/routes'
 import { scrollToElement, scrollToTop } from '@/lib/scroll'
 
-function ScrollToHashElement() {
+functiottn ScrollToHashElement() {
   const { pathname, hash } = useLocation()
 
   useEffect(() => {
